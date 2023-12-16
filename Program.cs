86de883527ea27bcb8a12d16d8dc05214a203047
Rashmi Priya <priye98priya@gmail.com>
2023-12-16 10:23:47 +0530

    Second commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Phase1Section3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doApp();
+        }
+        static public void doApp()
+        {
+
+            bool xbool = true;
+            byte xbyte = 250;
+            sbyte xsbyte = -100;
+            char xchar = 'a';
+            decimal xdecimal = 87.9M;
+            double xdouble = 651.09;
+            float xfloat = 465.1f;
+            int xint = -8909;
+            uint xuint = 91881;
+            long xlong = -1313131312;
+            ulong xulong = 01919181;
+            object xobject = new object();
+            short xshort = -19;
+            ushort xushort = 19;
+            string xstring = "name";
+
+           // if (xbool == true && xbyte < 300)
+                Console.WriteLine("This expression evaluates to true");
+            if (xshort > 6 || xint > -9000)
+                Console.WriteLine("This expression evaluates to false");
+
+
+
+
+
+            //Display results
+            Console.WriteLine($"bool: {xbool}");
+            Console.WriteLine($"byte: {xbyte}");
+            Console.WriteLine($"sbyte: {xsbyte}");
+            Console.WriteLine($"char: {xchar}");
+            Console.WriteLine($"decimal: {xdecimal}");
+            Console.WriteLine($"double: {xdouble}");
+            Console.WriteLine($"float: {xfloat}");
+            Console.WriteLine($"int: {xint}");
+            Console.WriteLine($"uint: {xuint}");
+            Console.WriteLine($"long: {xlong}");
+            Console.WriteLine($"ulong: {xulong}");
+            Console.WriteLine($"object: {xobject}");
+            Console.WriteLine($"short: {xshort}");
+            Console.WriteLine($"ushort: {xushort}");
+            Console.WriteLine($"string: {xstring}");
+            Console.WriteLine($"byte: {xbyte}");
+            //Console.WriteLine($"");
+
+
+
+
+        }
+
+    }
+}
