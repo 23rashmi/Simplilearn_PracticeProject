1edebe7b9106b71499350afb2ade59643a71460c
Rashmi Priya <91412524+23rashmi@users.noreply.github.com>
2023-12-16 11:38:17 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
-namespace Phase1Section3._4
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section3._6
 {
     class Program
     {
@@ -7,57 +12,45 @@ namespace Phase1Section3._4
         {
             doApp();
         }
-        static public void doApp()
+
+
+        public static void doApp()
         {
+            int x = 10;
+            if (x >= 10)
+                Console.WriteLine("This is a conditional statement.");
+            switch (x)
+            {
+                case 9:
+                    Console.WriteLine("This is a switch statement resulting in 9");
+                    break;
+                case 10:
+                    Console.WriteLine("This is a switch statement resulting in 10");
+                    break;
+            }
 
-            bool xbool = true;
-            byte xbyte = 250;
-            sbyte xsbyte = -100;
-            char xchar = 'a';
-            decimal xdecimal = 87.9M;
-            double xdouble = 651.09;
-            float xfloat = 465.1f;
-            int xint = -8909;
-            uint xuint = 91881;
-            long xlong = -1313131312;
-            ulong xulong = 01919181;
-            object xobject = new object();
-            short xshort = -19;
-            ushort xushort = 19;
-            string xstring = "name";
+            while (x < 20)
+            {
+                x++;
+                Console.WriteLine("Incrementing x in a while loop:" + x);
+            }
 
-           // if (xbool == true && xbyte < 300)
-                Console.WriteLine("This expression evaluates to true");
-            if (xshort > 6 || xint > -9000)
-                Console.WriteLine("This expression evaluates to false");
+            do
+            {
+                x--;
+                Console.WriteLine("Decrementing x in a do-while loop:" + x);
+            } while (x >= 10);
 
+            for (int i = 0; i <= x; i++)
+            {
+                Console.WriteLine("For loop to assign a value: " + i);
+            }
 
-
-
-
-            //Display results
-            Console.WriteLine($"bool: {xbool}");
-            Console.WriteLine($"byte: {xbyte}");
-            Console.WriteLine($"sbyte: {xsbyte}");
-            Console.WriteLine($"char: {xchar}");
-            Console.WriteLine($"decimal: {xdecimal}");
-            Console.WriteLine($"double: {xdouble}");
-            Console.WriteLine($"float: {xfloat}");
-            Console.WriteLine($"int: {xint}");
-            Console.WriteLine($"uint: {xuint}");
-            Console.WriteLine($"long: {xlong}");
-            Console.WriteLine($"ulong: {xulong}");
-            Console.WriteLine($"object: {xobject}");
-            Console.WriteLine($"short: {xshort}");
-            Console.WriteLine($"ushort: {xushort}");
-            Console.WriteLine($"string: {xstring}");
-            Console.WriteLine($"byte: {xbyte}");
-            //Console.WriteLine($"");
-
-
-
-
+            string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+            foreach (string s in numbers)
+            {
+                Console.WriteLine("Foreach loop to iterate through an array :" + s);
+            }
         }
-
     }
 }
